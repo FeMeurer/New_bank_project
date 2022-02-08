6aca1e6bf10e8abcc0735c828d49d0be446bae07
Felix Meurer <f.meurer@telekom.de>
2022-02-08 15:07:21 +0100

    Update

--- src/main/java/de/telekom/sea7/impl/ApplicationImpl.java
+++ src/main/java/de/telekom/sea7/impl/ApplicationImpl.java
@@ -1,13 +1,17 @@
+
 package de.telekom.sea7.impl;
 
-import java.sql.Connection;
+import java.math.RoundingMode;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.Properties;
 import java.util.Scanner;
 
-import de.telekom.sea7.impl.model.DBConnectionImpl;
-import de.telekom.sea7.impl.model.TransactionListTableImpl;
+import de.telekom.sea7.impl.model.GenericListImpl;
+import de.telekom.sea7.impl.model.TransactionImpl;
 import de.telekom.sea7.impl.view.TransactionListViewImpl;
-import de.telekom.sea7.inter.model.DBConnection;
-import de.telekom.sea7.inter.model.TransactionListTable;
+import de.telekom.sea7.inter.model.GenericList;
+import de.telekom.sea7.inter.model.Transaction;
 import de.telekom.sea7.inter.view.TransactionListView;
 
 public class ApplicationImpl extends BaseObjectImpl implements Application {
@@ -18,19 +22,45 @@ public class ApplicationImpl extends BaseObjectImpl implements Application {
 
 	@Override
 	public void run() {
-		DBConnection dbconnection = new DBConnectionImpl(this, "admin", "sea13", "mariadb", "localhost", "3306", "bank");
 		try {
-			try (Connection connection = dbconnection.getConnection()) {
-				try (TransactionListTable transactionListTable = new TransactionListTableImpl(this, connection)) { 
+			try (Connection connection = getConnection()) {
+				try (Statement stmt = connection.createStatement()) {
+					GenericList<Transaction> transactionList = new GenericListImpl<Transaction>(this);
+					ResultSet rs = stmt.executeQuery("select * from showTransactions;");
+					while (rs.next()) {
+						String name = rs.getString("name");
+						String iban = rs.getString("iban");
+						String bic = rs.getString("bic");
+						float amount = rs.getBigDecimal("amount").setScale(2, RoundingMode.DOWN).floatValue();
+						String purpose = rs.getString("purpose");
+						LocalDateTime date = rs.getTimestamp("date").toLocalDateTime();
+						Transaction transaction = new TransactionImpl(this, amount ,name  , iban , bic , purpose , date);
+						transactionList.add(transaction);
+					}
 					try (Scanner scanner = new Scanner(System.in)) {
-						TransactionListView transactionListView = new TransactionListViewImpl(this, scanner, transactionListTable.getTransactionList());
+						TransactionListView transactionListView = new TransactionListViewImpl(this, scanner,
+								transactionList);
 						transactionListView.menu();
 					}
 				}
+
+			
+			
 			}
-		} 
-		catch (Exception e) {
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+
+	}
+
+	public Connection getConnection() throws SQLException {
+
+		Properties connectionProps = new Properties();
+		connectionProps.put("user", "admin");
+		connectionProps.put("password", "toll");
+		Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/bank", connectionProps);
+		System.out.println("Connected to database");
+		return conn;
+
 	}
 }
